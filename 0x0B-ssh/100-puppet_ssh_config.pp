17d251f5562d3cb644b1e40446a2e1bb9043ac24
Marcosozhol <3956@holbertonschool.com>
2022-04-19 23:58:17 -0300

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,13 @@
 # Let’s practice using Puppet to make changes to our configuration file
 include stdlib
 file_line { 'Declare identity file':
-  path     => '/etc/ssh/ssh_config',
-  line     => '    IdentityFile ~/.ssh/scholl',
-  replace  => true,
+  path      => '/etc/ssh/ssh_config',
+  line      => '    IdentityFile ~/.ssh/scholl',
+  replace   => true,
 }
 
 file_line { 'Turn off passwd auth':
-  path     => '/etc/ssh/ssh_config',
-  line     => '    PasswordAuthentication no',
-  replace  => true,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
