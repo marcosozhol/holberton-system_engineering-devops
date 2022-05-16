85320722b0c2578a72fe2b9763e349a6b2ead52b
Marcosozhol <3956@holbertonschool.com>
2022-05-16 20:20:40 -0300

    Correction errro

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,9 +7,9 @@ returns information about his/her TODO list progress
 import requests
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "user/{}".format(sys.argv[1])).json()
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "user/{}/todos".format(sys.argv[1])).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
