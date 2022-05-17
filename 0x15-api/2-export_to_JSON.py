2a68b53f19a45c9623a364a2b5b8d495f8a97029
Marcosozhol <3956@holbertonschool.com>
2022-05-16 22:21:33 -0300

    Correction error

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
             "task": t.get("title"),
-            "completed": t.get("completed")
+            "completed": t.get("completed"),
             "username": username
         } for t in todos]}, jsonfile)
  
