18dcd95cd5eec024ddafa28f9bb4306e182e1b25
Marcosozhol <3956@holbertonschool.com>
2022-05-16 23:26:45 -0300

    Correction Task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+export data in the JSON format
+"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    # gets all users
+    users = requests.get("{}users".format(url)).json()
+    # jasonizer the users
+    file = {}
+    for user in users:
+        userId = user.get("id")
+        username = user.get("username")
+        todos = requests.get("/{}users/{}/todos".format(url, userId)).json()
+        all_task = [{"username": username,
+                     "task": all_task.get("title"),
+                     "completed": all_task.get("completed"),
+                     } for all_task in todos]
+        file[userId] = all_task
+        # to each user, add a key value pair
+        with open("todo_all_employees.json", "w") as filejs:
+            json.dump(file, filejs)
