55df9e0fda047169e06201066355c26cce50a78d
Marcosozhol <3956@holbertonschool.com>
2022-06-03 02:08:47 -0300

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and returns the number of subscribers"""
+
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """Return the total number of sebscribers on a given subreddit"""
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+    
+    return sub_info.json().get("data").get("subscribers")
