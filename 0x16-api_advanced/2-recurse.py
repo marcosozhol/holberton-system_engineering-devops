02bcc24815f34ef43d959596257f62d26914e797
Marcosozhol <3956@holbertonschool.com>
2022-06-03 02:48:59 -0300

    Correction error

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -19,15 +19,15 @@ def recurse(subreddit, hot_list=[], count=0, after=None):
                             allow_redirects=False)
     if sub_info.status_code >= 400:
         return None
-    
+
     hot_1 = hot_list + [child.get("data").get("title")
                         for child in sub_info.json()
                         .get("data")
                         .get("children")]
-    
+
     info = sub_info.json()
     if not info.get("data").get("after"):
         return hot_1
-    
+
     return recurse(subreddit, hot_1, info.get("data").get("count"),
                    info.get("data").get("after"))
