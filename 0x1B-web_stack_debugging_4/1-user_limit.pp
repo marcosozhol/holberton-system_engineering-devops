d0b4945e2c49ba30efcce1176d42c76f7b245be0
Marcosozhol <3956@holbertonschool.com>
2022-06-08 10:16:17 -0300

    Task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Change the settings to log in with holberton
+
+# Increase hard file limit
+exec { 'increase-hard-file':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit
+exec { 'increase-soft-file':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
